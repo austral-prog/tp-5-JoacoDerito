35ee259ab3fe2ff1e2efaa2112c7eb03d7a5ac7e
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-04-20 10:04:36 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,11 +1,15 @@
 def max_of_two(x, y):
+
     if x>y:
         return x
     elif x<y:
         return y
     elif x==y:
         return x
+
+
 def max_of_three(x, y, z):
+
     if (x>=y and x>z) or (x>y and x>=z):
         return x
     elif (y>=x and y>z) or (y>x and y>=z):
@@ -14,9 +18,15 @@ def max_of_three(x, y, z):
         return z
     elif x==y==z:
         return x
+    
 print(max_of_two(5, 4))
+
 print(max_of_two(-2,-3))
+
 print(max_of_two(0,0))
+
 print(max_of_three(5,4,7))
+
 print(max_of_three(-2,-3,-1))
+
 print(max_of_three(0,0,0))
