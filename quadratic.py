4286f3fa71626c15085cc1451ff797a1cd48452f
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-05-16 20:41:18 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,17 +1,24 @@
+from math import sqrt
 def roots(a, b, c):
-    discriminant = b**2 - 4*a*c
-    if discriminant > 0:
-        r1 = (-b + (discriminant)**0.5) / (2*a)
-        r2 = (-b - (discriminant)**0.5) / (2*a)
-        return "(" + str(r1) + ", " + str(r2) + ")"
-    elif discriminant == 0:
-        r1 = -b / (2*a)
-        return "(" + str(r1) + ")"
-    else:
+    coefficient = b**2 - 4 * a * c
+    if coefficient < 0:
         return "( )"
+    root1 = (-b + sqrt(coefficient)) / 2
+    root2 = (-b - sqrt(coefficient)) / 2
+    if (root1 == root2): return f"({root1})"
+    return f"({root1}, {root2})"
 def value_y(a, b, c, x):
-    return a*x**2 + b*x + c
+    return a * (x**2) + b * x + c
 def to_string(a, b, c):
-    return "f(x) = " + str(a) + " * X^2 + " + str(b) + " * X + " + str(c)
-def derivation(a, b):
-    return "f'(x) = " + str(2*a) + "x + " + str(b)
+    response = "f(x) ="
+    if a: response += f" {a} * X^2"
+    if b: response += f"{' + ' if a else ' '}{b} * X"
+    if c: response += f"{' + ' if a or b else ' '}{c}"
+    return response
+def derivation(a, b, _):
+    response = f"f'(x) = "
+    if not (a or b): return response + "0"
+    if (a): response += f"{a * 2} * X"
+    if (b): 
+        response += f"{' + ' if a else ''}{b}"
+    return response
